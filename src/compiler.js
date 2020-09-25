40317de8b0e0bef5fe5b2ed03c96667007d0ddd1
chenhan1 <chenhandoing@gmail.com>
2020-09-25 13:50:39 +0800

    perf: update module path rules
    update find module path rules

--- src/compiler.js
+++ src/compiler.js
@@ -2,8 +2,7 @@ const path = require('path');
 const parser = require('@babel/parser');
 const traverse = require('@babel/traverse').default;
 const babel = require('@babel/core');
-const packageNameRegex = require('package-name-regex');
-const findNodeModules = require('find-node-modules');
+const resolve = require('resolve');
 const Module = require('./Module.js');
 
 module.exports = class compiler {
@@ -13,39 +12,32 @@ module.exports = class compiler {
   }
   compile() {
     const fileName = this.fileName;
+    const dirname = path.dirname(fileName);
     const dependencies = {};
+
     const ast = parser.parse(this.content, {
       sourceType: 'module',
     });
-    traverse(ast, {
-      ImportDeclaration({ node }) {
-        const dirName = path.dirname(fileName);
-        if (path.extname(node.source.value)) {
-          if (node.source.value.charAt(0) === '.' || node.source.value.charAt(0) === '/') {
-            const depPath = path.join(dirName, node.source.value);
-            dependencies[node.source.value] = depPath;
-          } else {
-            const [nodeModulesPath] = findNodeModules({ cwd: process.cwd(), relative: false });
-            const moduleRootPath = path.resolve(nodeModulesPath, node.source.value);
-            dependencies[node.source.value] = moduleRootPath;
-          }
-        }
-        if (packageNameRegex.test(node.source.value)) {
-          const [nodeModulesPath] = findNodeModules({ cwd: process.cwd(), relative: false });
-          const moduleRootPath = path.resolve(nodeModulesPath, node.source.value);
-          const modulePackagePath = path.join(moduleRootPath, 'package.json');
-          const moduleEntryFilePath = require(modulePackagePath).main;
-          dependencies[node.source.value] = path.join(moduleRootPath, moduleEntryFilePath);
-        }
-      },
-    });
+
+    traverse(ast, this.dependenciesCollection(dependencies, dirname));
+
     const { code } = babel.transformFromAst(ast, null, {
       presets: ['@babel/preset-env'],
     });
+
     return new Module({
       fileName,
       dependencies,
       code,
     });
+  }
+  dependenciesCollection(dependencies, dirname) {
+    return {
+      ImportDeclaration({ node }) {
+        const moduleId = node.source.value;
+        const modulePath = resolve.sync(moduleId, { basedir: dirname });
+        dependencies[moduleId] = modulePath;
+      },
+    }
   }
 };
